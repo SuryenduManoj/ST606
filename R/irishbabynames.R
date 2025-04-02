7626056e80c8632e01cef26082a43c07aa8fa1df
SuryenduManoj <MATHSTATDS\24251159@rstudio.maths.nuim.ie>
2025-04-02 19:17:17 +0100

    initial commit

---
+++ R/irishbabynames.R
@@ -0,0 +1,12 @@
+#' Irish Baby Names Dataset
+#'
+#' A dataset containing Irish baby names and their counts from year 1964 to 2024.
+#'
+#' @format A data frame with 12,854 rows and 64 columns:
+#' \describe{
+#'   \item{Names}{Character: Name of the baby}
+#'   \item{Gender}{Factor: Male or Female}
+#'   \item{Statistic}}{a factor with levels Girls and Boys Names in Ireland with 3 or More Occurrences  and Rank}
+#' }
+#' @source Central Statistics Office of Ireland
+"irishbabynames"
