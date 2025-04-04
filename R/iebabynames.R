720d2d2578084752d970c39fda8546b8148873ab
shivani <shivani@shivanis-air.home>
2025-04-04 03:10:35 +0100

    commit

--- R/iebabynames.R
+++ R/iebabynames.R
@@ -1,12 +1,16 @@
 #install.packages("usethis")
 usethis::use_package("usethis")
 usethis::use_package("csodata")
+usethis::use_package("dplyr")
+usethis::use_package("devtools")
+usethis::use_package("roxygen2")
 
 
-girls <- cso_get_data("VSA60")
 
 
-boys <- cso_get_data("VSa50")
+girls <- csodata::cso_get_data("VSA60")
+boys <- csodata::cso_get_data("VSa50")
+
 
 colnames(boys) <- colnames(girls)  # Rename boys' columns to match girls'
 
@@ -14,19 +18,22 @@ girls$Gender <- "Female"
 boys$Gender <- "Male"
 irishbabynames <- rbind(girls, boys)
 
-usethis::use_package("dplyr")
+
 irishbabynames <- dplyr::rename(irishbabynames, Names = Girls.Names)
-save(irishbabynames, file = "data/irishbabynames.rda")
 
 
-usethis::use_package("devtools")
+
+
 
 #create_package("C:\\Users\\hp\\Documents\\tril_st606\\st606")
 #use_git()
-usethis::use_data(irishbabynames,overwrite = T)
+#usethis::use_data(irishbabynames,overwrite = T)
 
-prompt(irishbabynames, "man/irishbabynames.Rd")
+#prompt(irishbabynames, "man/irishbabynames.Rd")
 
-usethis::use_r("irishbabynames")
-load_all()
+#usethis::use_r("irishbabynames")
+
+
+
+
 
