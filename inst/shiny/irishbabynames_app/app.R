7e2897da35edeb8102e45ed9fdd2bb2af2e82f11
SuryenduManoj <suryendumanoj@gmail.com>
2025-06-04 02:09:42 +0100

    initial commit

--- inst/shiny/irishbabynames_app/app.R
+++ inst/shiny/irishbabynames_app/app.R
@@ -3,16 +3,41 @@ library(ggiraph)
 library(irishbabynames)  # Load your package
 
 ui <- fluidPage(
-  titlePanel("Irish Baby Names Dashboard"),
+  titlePanel("Irish Baby Names Dashboard"),sidebarPanel(
+    textInput("name", "Enter a Name", value = "Ci"),
+    selectInput("gender", "Gender", choices = c("Girls", "Boys")),
+    checkboxInput("per_million", "Normalize by per million births", value = FALSE),
+    sliderInput("top_n", "Number of Top Similar Names to Plot", min = 1, max = 10, value = 5)
+  ),
   mainPanel(
-    girafeOutput("plot")
+    ggiraphOutput("similarPlot", width = "100%", height = "600px")
   )
 )
 
-server <- function(input, output) {
-  output$plot <- renderGirafe({
-    plot_top10_girl_rank_lines(irishbabynames)
+# ---- Server ----
+server <- function(input, output, session) {
+  output$similarPlot <- renderggiraph({
+    req(input$name)
+
+    if (input$gender == "Girls") {
+      plot_similar_names_girls_ggiraph(
+        input_name = input$name,
+        max_distance = 1,  # Fixed internally
+        per_million = input$per_million,
+        top_n = input$top_n
+      )
+    } else {
+      plot_similar_names_boys_ggiraph(
+        input_name = input$name,
+        max_distance = 1,
+        per_million = input$per_million,
+        top_n = input$top_n
+      )
+    }
   })
 }
+
+# ---- Run App ----
+shinyApp(ui, server)
 
 shinyApp(ui = ui, server = server)
