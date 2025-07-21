9dad4cec8e49901226118c59529d64b4e40a42d9
SuryenduManoj <suryendumanoj@gmail.com>
2025-07-21 18:37:23 +0100

    commit for vignette

---
+++ doc/irishbabynames.R
@@ -0,0 +1,27 @@
+## ----include = FALSE----------------------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>"
+)
+
+## ----setup--------------------------------------------------------------------
+library(irishbabynames)
+library(ggiraph)
+library(plotly)
+data("irishbabynames")
+
+## ----eval = FALSE-------------------------------------------------------------
+# plot_trend("Aoife")
+
+## ----eval = FALSE-------------------------------------------------------------
+# plot_similar_girlsnames("Aoife")
+
+## ----eval = FALSE-------------------------------------------------------------
+# plot_similar_boysnames("John")
+
+## ----eval = FALSE-------------------------------------------------------------
+# plot_unisex_names("A","2024")
+
+## ----eval = FALSE-------------------------------------------------------------
+# irishbabynames_dashboard()
+
